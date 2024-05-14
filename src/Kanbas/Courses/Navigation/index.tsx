0e0017484cc52cd562d7ee3649bed952d6fd27cb
Ariane Uwase <arianeuwase@Arianes-MacBook-Pro.local>
2024-05-14 02:08:02 -0400

    update

--- src/Kanbas/Courses/Navigation/index.tsx
+++ src/Kanbas/Courses/Navigation/index.tsx
@@ -1,78 +1,16 @@
-import React from 'react';
-
-export default function Courses() {
-  return (
-    <div id="wd-courses">
+export default function CoursesNavigation() {
+    return (
       <ul id="wd-courses-navigation">
         <li><a id="wd-course-home-link"    href="#/Kanbas/Courses/1234/Home">Home</a></li>
-        <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/1234/Modules">Modules</a></li>
+        <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/1234/Modules">Modules
+          </a></li>
         <li><a id="wd-course-piazza-link"  href="#/Kanbas/Courses/1234/Piazza">Piazza</a></li>
         <li><a id="wd-course-zoom-link"    href="#/Kanbas/Courses/1234/Zoom">Zoom</a></li>
-        <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/1234/Assignments">Assignments</a></li>
-        <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/1234/Quizzes">Quizzes</a></li>
+        <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/1234/Assignments">
+            Assignments</a></li>
+        <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/1234/Quizzes">Quizzes
+          </a></li>
         <li><a id="wd-course-grades-link"  href="#/Kanbas/Courses/1234/Grades">Grades</a></li>
       </ul>
-
-      <ul id="wd-courses-navigation">
-        <li><a id="wd-course-home-link"    href="#/Kanbas/Courses/6120/Home">Home</a></li>
-        <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/6120/Modules">Modules</a></li>
-        <li><a id="wd-course-piazza-link"  href="#/Kanbas/Courses/6120/Piazza">Piazza</a></li>
-        <li><a id="wd-course-zoom-link"    href="#/Kanbas/Courses/6120/Zoom">Zoom</a></li>
-        <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/6120/Assignments">Assignments</a></li>
-        <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/6120/Quizzes">Quizzes</a></li>
-        <li><a id="wd-course-grades-link"  href="#/Kanbas/Courses/6120/Grades">Grades</a></li>
-      </ul>
-
-      <ul id="wd-courses-navigation">
-        <li><a id="wd-course-home-link"    href="#/Kanbas/Courses/7100/Home">Home</a></li>
-        <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/7100/Modules">Modules</a></li>
-        <li><a id="wd-course-piazza-link"  href="#/Kanbas/Courses/7100/Piazza">Piazza</a></li>
-        <li><a id="wd-course-zoom-link"    href="#/Kanbas/Courses/7100/Zoom">Zoom</a></li>
-        <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/7100/Assignments">Assignments</a></li>
-        <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/7100/Quizzes">Quizzes</a></li>
-        <li><a id="wd-course-grades-link"  href="#/Kanbas/Courses/7100/Grades">Grades</a></li>
-      </ul>
-
-      <ul id="wd-courses-navigation">
-        <li><a id="wd-course-home-link"    href="#/Kanbas/Courses/5140/Home">Home</a></li>
-        <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/5140/Modules">Modules</a></li>
-        <li><a id="wd-course-piazza-link"  href="#/Kanbas/Courses/5140/Piazza">Piazza</a></li>
-        <li><a id="wd-course-zoom-link"    href="#/Kanbas/Courses/5140/Zoom">Zoom</a></li>
-        <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/5140/Assignments">Assignments</a></li>
-        <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/5140/Quizzes">Quizzes</a></li>
-        <li><a id="wd-course-grades-link"  href="#/Kanbas/Courses/5140/Grades">Grades</a></li>
-      </ul>
-
-      <ul id="wd-courses-navigation">
-        <li><a id="wd-course-home-link"    href="#/Kanbas/Courses/5262/Home">Home</a></li>
-        <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/5262/Modules">Modules</a></li>
-        <li><a id="wd-course-piazza-link"  href="#/Kanbas/Courses/5262/Piazza">Piazza</a></li>
-        <li><a id="wd-course-zoom-link"    href="#/Kanbas/Courses/5262/Zoom">Zoom</a></li>
-        <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/5262/Assignments">Assignments</a></li>
-        <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/5262/Quizzes">Quizzes</a></li>
-        <li><a id="wd-course-grades-link"  href="#/Kanbas/Courses/5262/Grades">Grades</a></li>
-      </ul>
-
-      <ul id="wd-courses-navigation">
-        <li><a id="wd-course-home-link"    href="#/Kanbas/Courses/7990/Home">Home</a></li>
-        <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/7990/Modules">Modules</a></li>
-        <li><a id="wd-course-piazza-link"  href="#/Kanbas/Courses/7990/Piazza">Piazza</a></li>
-        <li><a id="wd-course-zoom-link"    href="#/Kanbas/Courses/7990/Zoom">Zoom</a></li>
-        <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/7990/Assignments">Assignments</a></li>
-        <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/7990/Quizzes">Quizzes</a></li>
-        <li><a id="wd-course-grades-link"  href="#/Kanbas/Courses/7990/Grades">Grades</a></li>
-      </ul>
-
-      <ul id="wd-courses-navigation">
-        <li><a id="wd-course-home-link"    href="#/Kanbas/Courses/7305/Home">Home</a></li>
-        <li><a id="wd-course-modules-link" href="#/Kanbas/Courses/7305/Modules">Modules</a></li>
-        <li><a id="wd-course-piazza-link"  href="#/Kanbas/Courses/7305/Piazza">Piazza</a></li>
-        <li><a id="wd-course-zoom-link"    href="#/Kanbas/Courses/7305/Zoom">Zoom</a></li>
-        <li><a id="wd-course-quizzes-link" href="#/Kanbas/Courses/7305/Assignments">Assignments</a></li>
-        <li><a id="wd-course-assignments-link" href="#/Kanbas/Courses/7305/Quizzes">Quizzes</a></li>
-        <li><a id="wd-course-grades-link"  href="#/Kanbas/Courses/7305/Grades">Grades</a></li>
-      </ul>
-
-    </div>
-  );
-}
+  );}
+  
