567f925a5eb138d669a33a66ff4a0f36e1a9d528
Ariane Uwase <arianeuwase@Arianes-MacBook-Pro.local>
2024-05-21 22:07:30 -0400

    update

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -24,7 +24,7 @@ export default function Courses() {
               <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<Assignments />} />
               <Route path="Assignments/:id" element={<AssignmentEditor/>} />
-              <Route path="Modules" element={<Modules />} />
+              <Route path="Grades" element={<Grades />} />
             </Routes>
           </td>
         </tr>
